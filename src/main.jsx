c73a5fa5b48e6edc88d1ce31fdd872f4112fcd9b
priyamkumar <priyamkumar976@gmail.com>
2024-10-12 10:50:47 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css'
 import Home from './components/Home.jsx';
-import Error from './components/Home.jsx';
+import Error from './components/Error.jsx';
 
 const router = createBrowserRouter([
   {
